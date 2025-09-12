ce6d45a06fa6873d1a266ceabc9c4629f61510c2
nganvtt74 <nganvtt74@gmail.com>
2025-09-12 19:39:38 +0700

    lab1/my_lesson1

---
+++ exercises/lab1/ai_pratice_prj/lession1/ex3_circle.py
@@ -0,0 +1,9 @@
+import math
+
+r = float(input("Nhập bán kính hình tròn: "))
+
+chu_vi = 2 * math.pi * r
+dien_tich = math.pi * r**2
+
+print("Chu vi hình tròn =", chu_vi)
+print("Diện tích hình tròn =", dien_tich)
